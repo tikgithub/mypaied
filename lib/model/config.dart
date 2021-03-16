d1d0dcf87d359e33939bdff43b2f3c41758fb977
tikgithub <tikgithub@gmail.com>
2021-03-16 10:30:46 +0700

    update

--- lib/model/config.dart
+++ lib/model/config.dart
@@ -1,7 +1,7 @@
 class Config {
   //http://192.168.0.7
   //https://mypaidserver.herokuapp.com
-  String hostname = 'http://192.168.0.7:8080/api/';
+  String hostname = 'http://192.168.56.1:8080/api/';
 
   String getHostName() {
     return hostname;
