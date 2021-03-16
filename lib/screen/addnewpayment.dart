39f732f9aa2085e9791c59558b69aa79ef51ea55
tikgithub <tikgithub@gmail.com>
2021-03-16 12:01:45 +0700

    updated

--- lib/screen/addnewpayment.dart
+++ lib/screen/addnewpayment.dart
@@ -9,7 +9,14 @@ class AddPayment extends StatefulWidget {
 }
 
 class _AddPaymentState extends State<AddPayment> {
+  //Define for using in showDateTimePicker()
   DateTime current = new DateTime.now();
+  //Define text controller
+  final TextEditingController txtItemController = TextEditingController();
+  final TextEditingController txtAmountController = TextEditingController();
+  final TextEditingController txtDetailController = new TextEditingController();
+  //Define Goboalkey for controlling the text
+  final formKey = GlobalKey<FormState>();
 
   Future<void> _selectDate(BuildContext ctx) async {
     final DateTime pickDate = await showDatePicker(
@@ -29,104 +36,130 @@ class _AddPaymentState extends State<AddPayment> {
     return Container(
         padding: EdgeInsets.all(10),
         child: Form(
+            key: formKey,
             child: Column(
-          children: [
-            TextFormField(
-              keyboardType: TextInputType.text,
-              decoration: InputDecoration(
-                labelText: 'Item',
-                prefixIcon: Icon(Icons.add_chart),
-              ),
-              style: TextStyle(
-                fontSize: 20,
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.only(top: 10),
-              child: TextButton(
-                onPressed: () {
-                  _selectDate(context);
-                },
-                child: Align(
-                  alignment: Alignment.topLeft,
-                  child: Row(children: [
-                    Icon(Icons.access_time),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Text(
-                      formatDate(current, [dd, '-', mm, '-', yyyy]) +
-                          '     (Press For Change)',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.black,
-                      ),
-                    ),
-                  ]),
+              children: [
+                TextFormField(
+                  autofocus: true,
+                  controller: txtItemController,
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                    labelText: 'Item',
+                    prefixIcon: Icon(Icons.add_chart),
+                  ),
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please add some text';
+                    }
+                    return null;
+                  },
                 ),
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.only(top: 10),
-              child: TextFormField(
-                keyboardType: TextInputType.multiline,
-                decoration: InputDecoration(
-                  labelText: 'Item Detail',
-                  prefixIcon: Icon(Icons.perm_device_information),
-                ),
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                ),
-                maxLines: null,
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.only(top: 10),
-              child: TextFormField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  labelText: 'Amount',
-                  prefixIcon: Icon(Icons.monetization_on),
-                ),
-                inputFormatters: [
-                  ThousandsFormatter(allowFraction: true),
-                ],
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                ),
-                maxLines: null,
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.only(top: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Container(
-                    padding: EdgeInsets.only(right: 10),
-                    child: OutlinedButton(
-                      onPressed: () {},
-                      child: Text('Clear'),
+                Container(
+                  padding: EdgeInsets.only(top: 10),
+                  child: TextButton(
+                    onPressed: () {
+                      _selectDate(context);
+                    },
+                    child: Align(
+                      alignment: Alignment.topLeft,
+                      child: Row(children: [
+                        Icon(Icons.access_time),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          formatDate(current, [dd, '-', mm, '-', yyyy]) +
+                              '     (Press For Change)',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ]),
                     ),
                   ),
-                  Container(
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: Text('Save'),
+                ),
+                Container(
+                  padding: EdgeInsets.only(top: 10),
+                  child: TextFormField(
+                    controller: txtDetailController,
+                    keyboardType: TextInputType.multiline,
+                    decoration: InputDecoration(
+                      labelText: 'Item Detail',
+                      prefixIcon: Icon(Icons.perm_device_information),
                     ),
-                  )
-                ],
-              ),
-            ),
-          ],
-        )));
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    maxLines: null,
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(top: 10),
+                  child: TextFormField(
+                    controller: txtAmountController,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: 'Amount',
+                      prefixIcon: Icon(Icons.monetization_on),
+                    ),
+                    inputFormatters: [
+                      ThousandsFormatter(allowFraction: true),
+                    ],
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    maxLines: null,
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please enter any amount';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.only(right: 10),
+                        child: OutlinedButton(
+                          onPressed: () {
+                            formKey.currentState.reset();
+                            // txtAmountController.clear();
+                            // txtDetailController.clear();
+                            // txtItemController.clear();
+                          },
+                          child: Text('Clear'),
+                        ),
+                      ),
+                      Container(
+                        child: ElevatedButton(
+                          onPressed: () {
+                            if (formKey.currentState.validate()) {
+                              formKey.currentState.save();
+                            }
+                          },
+                          child: Text('Save'),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            )));
   }
 
   @override
