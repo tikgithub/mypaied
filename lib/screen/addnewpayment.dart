e3c57b2821ed226e644cb8a9daa4cd8a0fe73b4b
tikgithub <@Computerz1>
2021-03-17 08:42:44 +0700

    updatge

--- lib/screen/addnewpayment.dart
+++ lib/screen/addnewpayment.dart
@@ -1,7 +1,16 @@
+import 'dart:ffi';
+import 'dart:io';
+import 'package:dio/dio.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:date_format/date_format.dart';
 import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:mypaied/model/config.dart';
 import 'package:pattern_formatter/pattern_formatter.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebaseStorage;
+import 'package:uuid/uuid.dart';
 
 class AddPayment extends StatefulWidget {
   @override
@@ -17,6 +26,14 @@ class _AddPaymentState extends State<AddPayment> {
   final TextEditingController txtDetailController = new TextEditingController();
   //Define Goboalkey for controlling the text
   final formKey = GlobalKey<FormState>();
+  //Image file
+  var imageFile;
+  var imageThumnailPath = 'images/avatar.png';
+  //Directory information
+  final baseFolder = "";
+
+  //Image source control
+  ImageSource imageSource;
 
   Future<void> _selectDate(BuildContext ctx) async {
     final DateTime pickDate = await showDatePicker(
@@ -128,28 +145,71 @@ class _AddPaymentState extends State<AddPayment> {
                     },
                   ),
                 ),
+                showPhotoSelection(),
                 Container(
                   margin: EdgeInsets.only(top: 20),
                   child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
                       Container(
                         padding: EdgeInsets.only(right: 10),
                         child: OutlinedButton(
                           onPressed: () {
                             formKey.currentState.reset();
-                            // txtAmountController.clear();
-                            // txtDetailController.clear();
-                            // txtItemController.clear();
+                            setState(() {
+                              imageFile = null;
+                              txtAmountController.clear();
+                              txtDetailController.clear();
+                              txtItemController.clear();
+                            });
                           },
                           child: Text('Clear'),
                         ),
                       ),
                       Container(
                         child: ElevatedButton(
-                          onPressed: () {
+                          onPressed: () async {
                             if (formKey.currentState.validate()) {
                               formKey.currentState.save();
+                              //Upload image to firebase server
+                              await Firebase.initializeApp();
+                              print('after');
+                              //check photo was select or note
+                              if (imageFile == null) {
+                                print('User not select image');
+                                imageFile = Image.asset('images/avatar.png');
+                              }
+                              //Generate filename of image
+                              String newFileName = Uuid().v1();
+                              firebaseStorage.Reference ref = firebaseStorage
+                                  .FirebaseStorage.instance
+                                  .ref()
+                                  .child('Items/$newFileName.png');
+                              await ref.putFile(imageFile);
+                              //Upload image file done
+                              //Send data to API
+
+                              var client = new Dio();
+                              client.options.headers['authorization'] =
+                                  'Bearer ' +
+                                      await FirebaseAuth.instance.currentUser
+                                          .getIdToken(true);
+                              Response res = await client.post(
+                                  Config().getHostName() + 'item/',
+                                  data: {
+                                    'title': txtItemController.text,
+                                    'payDate': formatDate(
+                                      current,
+                                      [yyyy, '-', mm, '-', dd],
+                                    ),
+                                    'detail': txtDetailController.text,
+                                    'photo': newFileName + '.png',
+                                    'amount': txtAmountController.text
+                                        .replaceAll(',', ''),
+                                    'email':
+                                        FirebaseAuth.instance.currentUser.email,
+                                  });
+                              print(res);
                             }
                           },
                           child: Text('Save'),
@@ -160,6 +220,91 @@ class _AddPaymentState extends State<AddPayment> {
                 ),
               ],
             )));
+  }
+
+  Future<void> showImageSelectionDialog() async {
+    await showDialog(
+        context: context,
+        builder: (BuildContext ctx) {
+          return AlertDialog(
+            actions: [
+              TextButton(
+                onPressed: () {
+                  imageSource = ImageSource.gallery;
+                  Navigator.of(context).pop();
+                },
+                child: Row(
+                  children: [
+                    Icon(Icons.photo),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text('Gallery')
+                  ],
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  imageSource = ImageSource.camera;
+                  Navigator.of(context).pop();
+                },
+                child: Row(
+                  children: [
+                    Icon(Icons.camera),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text('Camera'),
+                  ],
+                ),
+              ),
+            ],
+            title: Text('Please select Image source'),
+          );
+        });
+  }
+
+  Widget showPhotoSelection() {
+    return Container(
+      margin: EdgeInsets.only(top: 10),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width / 2,
+                child: GestureDetector(
+                  onTap: () async {
+                    //Set image source to null when tap photo select
+                    imageSource = null;
+                    //Show image selection photo dialog
+                    await showImageSelectionDialog();
+                    //check image was selected ?
+                    if (imageSource != null) {
+                      //Show image in thumnail'
+                      var imageData = await ImagePicker()
+                          .getImage(source: imageSource, imageQuality: 50);
+                      setState(() {
+                        imageFile = File(imageData.path);
+                      });
+                    }
+                  },
+                  child: imageFile == null
+                      ? Image.asset(
+                          imageThumnailPath,
+                        )
+                      : Image.file(imageFile),
+                ),
+              ),
+            ],
+          ),
+          Container(
+            child: Text('Tap to select photo'),
+          ),
+        ],
+      ),
+    );
   }
 
   @override
